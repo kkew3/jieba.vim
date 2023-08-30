db3e2a211f609ae2d4c7c73686945dc453eeead1
kkew3 <kps6326@hotmail.com>
2023-08-30 18:25:37 +0800

    Added custom jieba initialize function that mutes logging.

--- pythonx/jieba_vim/__init__.py
+++ pythonx/jieba_vim/__init__.py
@@ -1,1 +1,10 @@
+import logging
+
+from .libjieba import setLogLevel as _setLogLevel
+from .libjieba import initialize as _initialize
 from .libjieba import cut as jieba_cut
+
+
+def jieba_initialize(dictionary=None):
+    _setLogLevel(logging.WARNING)
+    _initialize(dictionary)
