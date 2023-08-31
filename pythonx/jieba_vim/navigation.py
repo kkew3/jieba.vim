8c3283c89eda3cb59d9b1a60fa280b6644d93a7f
kkew3 <kps6326@hotmail.com>
2023-08-31 15:14:50 +0800

    Responded to v:count and visual mode.

--- pythonx/jieba_vim/navigation.py
+++ pythonx/jieba_vim/navigation.py
@@ -4,7 +4,29 @@ from .pysrc import navigation
 from .init import jieba_initialized
 
 
+def responding_to_vcount(func):
+    def _wrapper():
+        for _ in range(int(vim.eval('v:count1'))):
+            func()
+
+    return _wrapper
+
+
+def in_visual_mode(func):
+    def _wrapper():
+        # Reference: https://stackoverflow.com/q/16212801/7881370
+        vim.command('normal! gv')
+        func()
+
+    return _wrapper
+
+
 @jieba_initialized
+@responding_to_vcount
 def backward_start():
     vim.current.window.cursor = navigation.backward_start(
         vim.current.buffer, vim.current.window.cursor)
+
+
+# known issue: responding_to_vcount won't work in visual mode
+backward_start_v = in_visual_mode(backward_start)
