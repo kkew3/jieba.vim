1d8def785e75d41631df0ba3f787f47643fb433d
kkew3 <kps6326@hotmail.com>
2024-08-23 19:04:57 +0800

    Download the cutword word list before build

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+set -e
+
+echo "=== Downloading the word list from cutword ==="
+mkdir -p pythonx/src/data
+curl -o pythonx/src/data/unionwords.txt "https://raw.githubusercontent.com/liwenju0/cutword/main/cutword/unionwords.txt"
+echo "=== Building rust ==="
 cd pythonx && cargo build -r
