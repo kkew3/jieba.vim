14f9f1a0280e84b047282dc4d48dea486119d32b
kkew3 <kps6326@hotmail.com>
2024-08-27 21:43:46 +0800

    Bugfix: fix cross-platform issue of pyo3 in `build.rs` (issue #1)
    See https://pyo3.rs/v0.22.2/building-and-distribution.html#manual-builds for detail.

--- pythonx/build.rs
+++ pythonx/build.rs
@@ -1,3 +1,4 @@
+use const_format::concatcp;
 use curl::easy::Easy;
 use std::io::Write;
 use std::{env, fs, io};
@@ -38,14 +39,43 @@ fn download_word_list_from_cutword(path: &str) {
     transfer.perform().unwrap();
 }
 
+// See https://pyo3.rs/v0.22.2/building-and-distribution.html#manual-builds
+// for detail about `SOURCE` and `DEST` in module `target_cfg`.
+
+const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+
+#[cfg(target_os = "windows")]
+mod target_cfg {
+    use const_format::concatcp;
+
+    pub const SOURCE: &str = concatcp!("lib", super::PKG_NAME, ".dll");
+    pub const DEST: &str = concatcp!(super::PKG_NAME, ".pyd");
+}
+
+#[cfg(target_os = "macos")]
+mod target_cfg {
+    use const_format::concatcp;
+
+    pub const SOURCE: &str = concatcp!("lib", super::PKG_NAME, ".dylib");
+    pub const DEST: &str = concatcp!(super::PKG_NAME, ".so");
+}
+
+#[cfg(target_os = "linux")]
+mod target_cfg {
+    use const_format::concatcp;
+
+    pub const SOURCE: &str = concatcp!("lib", super::PKG_NAME, ".so");
+    pub const DEST: &str = concatcp!(super::PKG_NAME, ".so");
+}
+
 fn main() {
     mkdir("src/data");
     download_word_list_from_cutword("src/data/unionwords.txt");
     pyo3_build_config::add_extension_module_link_args();
     if env::var("PROFILE").unwrap() == "release" {
         env::set_current_dir("jieba_vim").unwrap();
-        let so = "jieba_navi_rs.so";
-        let dylib_src = "../target/release/libjieba_navi_rs.dylib";
+        let so = target_cfg::DEST;
+        let dylib_src = concatcp!("../target/release/", target_cfg::SOURCE);
         match ensure_not_exists(so) {
             Ok(()) => symlink::symlink_file(dylib_src, so).unwrap(),
             Err(()) => panic!(
