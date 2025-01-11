a0bb9937643eef9692042ac22d2236c7c898e6ac
kkew3 <kps6326@hotmail.com>
2025-01-11 22:25:58 +0800

    Add test runner, and convert existing test cases to the new format

---
+++ test/test_jieba.py
@@ -0,0 +1,54 @@
+import os
+from typing import Literal
+import subprocess
+
+from jinja2 import Environment, FileSystemLoader
+
+BASEDIR = 'cases'
+
+
+def clean_vader():
+    for name in os.listdir(BASEDIR):
+        if name.endswith('.vader'):
+            os.remove(os.path.join(BASEDIR, name))
+
+
+def compile_j2(vim_bin: Literal['vim', 'nvim'], verify_only: bool):
+    env = Environment(loader=FileSystemLoader(BASEDIR))
+    vim = (vim_bin == 'vim')
+    nvim = (vim_bin == 'nvim')
+    for name in os.listdir(BASEDIR):
+        if name.endswith('.vader.j2'):
+            template = env.get_template(name)
+            file = 'verify_{}'.format(name[:-3])
+            with open(
+                    os.path.join(BASEDIR, file), 'w',
+                    encoding='utf-8') as outfile:
+                outfile.write(template.render(vim=vim, nvim=nvim, verify=True))
+            if not verify_only:
+                file = 'test_{}'.format(name[:-3])
+                with open(
+                        os.path.join(BASEDIR, file), 'w',
+                        encoding='utf-8') as outfile:
+                    outfile.write(
+                        template.render(vim=vim, nvim=nvim, verify=False))
+
+
+def eval_with_vim(vim_bin: Literal['vim', 'nvim']):
+    proc = subprocess.run([
+        vim_bin, '-u', 'vimrc', '-c', 'silent Vader! {}'.format(
+            os.path.join(BASEDIR, '*.vader'))
+    ],
+                          capture_output=True,
+                          text=True)
+    if proc.returncode != 0:
+        assert False, proc.stderr
+
+
+def test_all():
+    vim_bin = os.environ['VIM_BIN_NAME']
+    verify_only = bool(int(os.environ.get('VERIFY_ONLY', '0')))
+    assert vim_bin in ('vim', 'nvim')
+    clean_vader()
+    compile_j2(vim_bin, verify_only)
+    eval_with_vim(vim_bin)
