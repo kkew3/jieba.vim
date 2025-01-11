426857aadc445886c835dbf92a979ee3be674fba
kkew3 <kps6326@hotmail.com>
2025-01-11 23:07:22 +0800

    Update venv location

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-[ ! -d "../venv" ] && python3 -m venv ../venv
-source ../venv/bin/activate
-pip install -q -r requirements.txt
+source venv/bin/activate
 echo "=== vim integration tests ==="
 VIM_BIN_NAME=vim pytest
 echo
