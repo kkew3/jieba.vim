kkew3/jieba.vim

commit 2e83b1ab4cbfdd72cce7b97202aa10af8e57102d

Bugfix: handle visual mode edge case
In visual mode, when the cursor is at one character after the last character of the buffer, ge would jump to the last but one word rather than the last word before the fix.

pythonx/jieba_vim/navigation.py
@@ -104,7 +104,15 @@ def _vim_wrapper_factory_x(motion_name):
         # multiple instances of Vim open?
         vim.command('let g:jieba_vim_previous_virtualedit = &virtualedit')
         vim.command('set virtualedit=onemore')
-        output = method(vim.current.buffer, vim.current.window.cursor, count)
+        # Handle the case where cursor is one character after the last
+        # character of the buffer in visual mode.
+        line = vim.current.window.cursor[0]
+        col_gt = int(vim.eval('''col("'>")''')) - 1
+        if col_gt >= len(vim.current.buffer[line - 1].encode('utf-8')):
+            output = method(vim.current.buffer, (line, col_gt), count)
+        else:
+            output = method(vim.current.buffer, vim.current.window.cursor,
+                            count)
         vim.current.window.cursor = output.cursor
 
     def _teardown_wrapper():

test/test_jieba_en.py
@@ -268,6 +268,7 @@ def eval_with_vim(vader_test_file: Path, unlink_on_success: bool = True):
 @hypothesis.example((['a'], 'o', [], ['"xy', 'w'], None))
 @hypothesis.example(([',', ',', ','], 'n', [], ['w'], None))
 @hypothesis.example((['a', 'a'], 'n', [], ['b', 'w'], None))
+@hypothesis.example((['a', 'a'], 'xchar', ['v'], ['w', 'ge'], ['"xy']))
 def test_jieba_en(args):
     paragraph, mode, setup_keys, jieba_keys, teardown_keys = args
     vader_test_file = write_vader_test(paragraph, mode, setup_keys, jieba_keys,

commit 7eb787baeed543299359e86a38354150909e5af8

Bugfix: fix bug introduced in commit 2e83b1ab4cbfdd72cce7b97202aa10af8e57102d
Commit 2e83b1ab4cbfdd72cce7b97202aa10af8e57102d attempted to fix an edge case in xmap ge motion, but introduced a severer issue, which is tackled in this commit. See test/cases/xmap_back_empty_line.vader.j2 for details.

pythonx/jieba_vim/navigation.py
@@ -105,13 +105,40 @@ def _vim_wrapper_factory_x(motion_name):
         vim.command('set virtualedit=onemore')
         # Handle the case where cursor is one character after the last
         # character of the buffer in visual mode.
-        line = vim.current.window.cursor[0]
+        #
+        # FIXME Current handling does not cover every edge case, especially
+        # the case where the cursor is at the last character of the buffer in
+        # visual line mode. I'm aware of it, and intentionally mark
+        # test/cases/xmap_ge_eol.vader.j2 as IGNORED. Generally speaking, the
+        # failed case (which involves motion `ge` in visual line mode) does not
+        # have severe impact to users, as `ge` is rarely used. Some plugins,
+        # e.g. preservim/vim-markdown, even remaps it to certain plugin
+        # function. Therefore, this might be fixed in the future, but with a
+        # relatively low priority.
+        line, col = vim.current.window.cursor
+        # True if the cursor needs repositioning. This condition is neither
+        # sufficient nor necessary, meaning that there will be false positives.
+        # Note, however, that without the fourth clause `visualmode() != "V"`,
+        # the condition becomes necessary. The fourth clause is added to reduce
+        # the number of false positives.
+        need_reposition_cursor = bool(
+            int(
+                vim.eval('''line("'>") == line("$") '''
+                         '''&& col("'>") == col("$") '''
+                         '''&& line(".") == line("'>") '''
+                         '''&& visualmode() != "V"''')))
         col_gt = int(vim.eval('''col("'>")''')) - 1
-        if col_gt >= len(vim.current.buffer[line - 1].encode('utf-8')):
+        if motion_name in ('ge', 'gE') and need_reposition_cursor:
+            # Reposition the cursor to (line, col_gt).
             output = method(vim.current.buffer, (line, col_gt), count)
+            # Since `ge` should move cursor backward, if the cursor results
+            # in a position after the previous position, it means the
+            # repositioning is a false positive. We will then rerun the
+            # function without repositioning to find the output cursor.
+            if output.cursor[0] == line and output.cursor[1] > col:
+                output = method(vim.current.buffer, (line, col), count)
         else:
-            output = method(vim.current.buffer, vim.current.window.cursor,
-                            count)
+            output = method(vim.current.buffer, (line, col), count)
         vim.current.window.cursor = output.cursor
         # The `m>gv` trick reference:
         # https://github.com/svermeulen/vim-NotableFt/blob/01732102c1d8c7b7bd6e221329e37685aa4ab41a/plugin/NotableFt.vim#L32

test/cases/IGNORED_xmap_ge_eol.vader.j2
@@ -1,0 +1,59 @@
+* This tests for a bug where in all visual modes, when the cursor is at the
+* position one character after the last character, motion ge does not go back
+* to the last character in the buffer.
+
+{% set keys = ["w", "W", "e", "E", "b", "B", "ge", "gE"] -%}
+{%- set bang = "!" if verify else "" -%}
+{%- set vim_compatible = true -%}
+{%- set nvim_compatible = true %}
+
+Before:
+  {%- for k in keys %}
+  map {{ k }} <Plug>(Jieba_{{ k }})
+  {%- endfor %}
+
+After:
+  {%- for k in keys %}
+  unmap {{ k }}
+  {%- endfor %}
+
+---
+
+{%- if (not vim or vim_compatible) and (not nvim or nvim_compatible) %}
+
+Given:
+  abc
+
+Execute (character):
+  normal! $
+  execute "normal{{ bang }} v$ge\<Esc>"
+
+Then:
+  AssertEqual 3, col("'<")
+  AssertEqual 3, col("'>")
+  AssertEqual 3, col(".")
+
+Execute (line):
+  normal! $
+  execute "normal{{ bang }} V$ge\<Esc>"
+
+Then:
+  AssertEqual 1, col("'<")
+  AssertEqual 4, col("'>")
+  AssertEqual 3, col(".")
+
+Execute (block):
+  normal! $
+  execute "normal{{ bang }} \<C-v>$ge\<Esc>"
+
+Then:
+  AssertEqual 3, col("'<")
+  AssertEqual 3, col("'>")
+  AssertEqual 3, col(".")
+
+{% endif %}
+
+---
+
+Before:
+After:

test/cases/xmap_back_empty_line.vader.j2
@@ -1,0 +1,271 @@
+* This tests for a bug where in all visual modes, backward motions cannot go up
+* beyond an empty line.
+
+{% set keys = ["w", "W", "e", "E", "b", "B", "ge", "gE"] -%}
+{%- set bang = "!" if verify else "" -%}
+{%- set vim_compatible = true -%}
+{%- set nvim_compatible = true %}
+
+Before:
+  {%- for k in keys %}
+  map {{ k }} <Plug>(Jieba_{{ k }})
+  {%- endfor %}
+
+After:
+  {%- for k in keys %}
+  unmap {{ k }}
+  {%- endfor %}
+
+---
+
+{%- if (not vim or vim_compatible) and (not nvim or nvim_compatible) %}
+
+Given (abc):
+  
+  
+  abc
+
+Execute (motion b, character, x1):
+  normal! G$
+  execute "normal{{ bang }} vb\<Esc>"
+
+Then:
+  AssertEqual 3, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, character, x2):
+  normal! G$
+  execute "normal{{ bang }} vbb\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, character, x3):
+  normal! G$
+  execute "normal{{ bang }} vbbb\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, block, x1):
+  normal! G$
+  execute "normal{{ bang }} \<C-v>b\<Esc>"
+
+Then:
+  AssertEqual 3, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, block, x2):
+  normal! G$
+  execute "normal{{ bang }} \<C-v>bb\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, block, x3):
+  normal! G$
+  execute "normal{{ bang }} \<C-v>bbb\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, line, x1):
+  normal! G$
+  execute "normal{{ bang }} Vb\<Esc>"
+
+Then:
+  AssertEqual 3, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, line, x2):
+  normal! G$
+  execute "normal{{ bang }} Vbb\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, line, x3):
+  normal! G$
+  execute "normal{{ bang }} Vbbb\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+---
+
+Given (abc):
+  
+  
+  abc
+
+Execute (motion ge, character, x1):
+  normal! G$
+  execute "normal{{ bang }} vge\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, character, x2):
+  normal! G$
+  execute "normal{{ bang }} vgege\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, block, x1):
+  normal! G$
+  execute "normal{{ bang }} \<C-v>ge\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, block, x2):
+  normal! G$
+  execute "normal{{ bang }} \<C-v>gege\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, line, x1):
+  normal! G$
+  execute "normal{{ bang }} Vge\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, line, x2):
+  normal! G$
+  execute "normal{{ bang }} Vgege\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+---
+
+Given (a):
+  
+  
+  a
+
+Execute (motion b, character, x1):
+  normal! G$
+  execute "normal{{ bang }} vb\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, character, x2):
+  normal! G$
+  execute "normal{{ bang }} vbb\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, block, x1):
+  normal! G$
+  execute "normal{{ bang }} \<C-v>b\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, block, x2):
+  normal! G$
+  execute "normal{{ bang }} \<C-v>bb\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, line, x1):
+  normal! G$
+  execute "normal{{ bang }} Vb\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion b, line, x2):
+  normal! G$
+  execute "normal{{ bang }} Vbb\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+---
+
+Given (a):
+  
+  
+  a
+
+Execute (motion ge, character, x1):
+  normal! G$
+  execute "normal{{ bang }} vge\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, character, x2):
+  normal! G$
+  execute "normal{{ bang }} vgege\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, block, x1):
+  normal! G$
+  execute "normal{{ bang }} \<C-v>ge\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, block, x2):
+  normal! G$
+  execute "normal{{ bang }} \<C-v>gege\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, line, x1):
+  normal! G$
+  execute "normal{{ bang }} Vge\<Esc>"
+
+Then:
+  AssertEqual 2, line("'<")
+  AssertEqual 1, col("'<")
+
+Execute (motion ge, line, x2):
+  normal! G$
+  execute "normal{{ bang }} Vgege\<Esc>"
+
+Then:
+  AssertEqual 1, line("'<")
+  AssertEqual 1, col("'<")
+
+
+{% endif %}
+
+---
+
+Before:
+After: