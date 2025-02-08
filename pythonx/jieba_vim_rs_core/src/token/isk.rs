4f8e73d50fee942c16def2f2338110ffa0371b69
kkew3 <kps6326@hotmail.com>
2025-02-08 16:49:45 +0800

    Add license

--- pythonx/jieba_vim_rs_core/src/token/isk.rs
+++ pythonx/jieba_vim_rs_core/src/token/isk.rs
@@ -1,3 +1,17 @@
+// Copyright 2025 Kaiwen Wu. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
 //! Figure out which letters are words, based on `'iskeyword'` Vim option.
 //! Why to use context-free grammar parser when the option value is regular?
 //! Because it's not possible to capture repetition of the same group, while at
