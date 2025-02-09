332dd09374eab3c50de89669919a933c19869383
kkew3 <kps6326@hotmail.com>
2025-02-09 20:56:38 +0800

    Add `KeywordCutter`, a `JiebaPlaceholder` implementation

--- pythonx/jieba_vim_rs_core/src/token/jieba.rs
+++ pythonx/jieba_vim_rs_core/src/token/jieba.rs
@@ -18,3 +18,83 @@ pub trait JiebaPlaceholder {
     /// Cut sentence with `hmm` enabled.
     fn cut_hmm<'a>(&self, sentence: &'a str) -> Vec<&'a str>;
 }
+
+#[cfg(test)]
+use std::collections::HashSet;
+
+#[cfg(test)]
+pub struct KeywordCutter {
+    dict: HashSet<String>,
+}
+
+#[cfg(test)]
+impl KeywordCutter {
+    pub fn new(dict: impl IntoIterator<Item = String>) -> Self {
+        Self {
+            dict: dict.into_iter().collect(),
+        }
+    }
+}
+
+#[cfg(test)]
+impl JiebaPlaceholder for KeywordCutter {
+    fn cut_hmm<'a>(&self, sentence: &'a str) -> Vec<&'a str> {
+        let mut i = 0;
+        let boundaries: Vec<_> = sentence
+            .char_indices()
+            .map(|(i, _)| i)
+            .chain([sentence.len()])
+            .collect();
+        let mut result = Vec::new();
+        let n_chars = boundaries.len();
+        while i < n_chars {
+            let mut found = false;
+            for j in (i + 1..=n_chars).rev() {
+                let segment = &sentence[boundaries[i]..boundaries[j]];
+                if self.dict.contains(segment) {
+                    result.push(segment);
+                    i = j;
+                    found = true;
+                    break;
+                }
+            }
+            if !found {
+                let mut j = i + 1;
+                let mut segment = &sentence[boundaries[i]..boundaries[j]];
+                while j < n_chars && !self.dict.contains(segment) {
+                    j += 1;
+                    segment = &sentence[boundaries[i]..boundaries[j]];
+                }
+                result.push(segment);
+                i = j;
+            }
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{JiebaPlaceholder, KeywordCutter};
+
+    #[test]
+    fn test_keyword_cutter() {
+        let kc =
+            KeywordCutter::new(["fooo".into(), "bar".into(), "你好".into()]);
+        assert!(kc.cut_hmm("").is_empty());
+        assert_eq!(kc.cut_hmm("bazz"), vec!["bazz"]);
+        assert_eq!(kc.cut_hmm("barr"), vec!["bar", "r"]);
+        assert_eq!(kc.cut_hmm("rbar"), vec!["r", "bar"]);
+        assert_eq!(kc.cut_hmm("foooquxbazbaz"), vec!["fooo", "quxbazbaz"]);
+        assert_eq!(
+            kc.cut_hmm("bazfooobarquxbazfoobar"),
+            vec!["baz", "fooo", "bar", "quxbazfoo", "bar"]
+        );
+        assert_eq!(kc.cut_hmm("你好你"), vec!["你好", "你"]);
+        assert_eq!(
+            kc.cut_hmm("foo你bar好你好"),
+            vec!["foo你", "bar", "好", "你好"]
+        );
+    }
+}
