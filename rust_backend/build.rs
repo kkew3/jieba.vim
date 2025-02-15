abde094f3bc7cdd5373693741e75143fee2b938f
kkew3 <kps6326@hotmail.com>
2025-02-15 17:59:53 +0800

    Move all rust-related stuff to /rust_backend; update build script and cicd

--- rust_backend/build.rs
+++ rust_backend/build.rs
@@ -5,7 +5,9 @@ fn main() {
     pyo3_build_config::add_extension_module_link_args();
     if env::var("PROFILE").unwrap() == "release" {
         let pypackage: PathBuf =
-            [env!("CARGO_MANIFEST_DIR"), "jieba_vim"].iter().collect();
+            [env!("CARGO_MANIFEST_DIR"), "../pythonx/jieba_vim"]
+                .iter()
+                .collect();
         manual_build_pyo3::expose_shared_library(pypackage).unwrap();
     }
 }
