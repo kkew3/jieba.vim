07f0c68d35bb5fabbe1a3e8f2691106ef3b1404c
kkew3 <kps6326@hotmail.com>
2025-02-15 18:51:19 +0800

    Skip exposing shared library to python when building it on github workflow cicd
    Also remove debugging line from github workflow cicd.

--- rust_backend/build.rs
+++ rust_backend/build.rs
@@ -3,6 +3,15 @@ use std::path::PathBuf;
 
 fn main() {
     pyo3_build_config::add_extension_module_link_args();
+    // Skip this step when building the final shared library on github. We
+    // have to skip this step because `cross` won't be able to find jieba_vim.
+    // Skipping this step is okay because in this case, exposing the shared
+    // library to python is not necessary.
+    if let Ok(value) = env::var("SKIP_EXPOSE_SHARED_LIBRARY_TO_PYTHON") {
+        if value == "1" {
+            return;
+        }
+    }
     if env::var("PROFILE").unwrap() == "release" {
         let pypackage: PathBuf =
             [env!("CARGO_MANIFEST_DIR"), "../pythonx/jieba_vim"]
