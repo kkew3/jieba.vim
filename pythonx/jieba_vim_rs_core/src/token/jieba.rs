27b1379b99a4b413d77a7f470b1c69d4d650bd28
kkew3 <kps6326@hotmail.com>
2025-02-15 16:30:37 +0800

    Move `KeywordCutter` to its own module
    So that we save writing several "#[cfg(test)]" and make the code cleaner.

--- pythonx/jieba_vim_rs_core/src/token/jieba.rs
+++ pythonx/jieba_vim_rs_core/src/token/jieba.rs
@@ -12,8 +12,7 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-//! This module defines abstraction over `Jieba`, and several implementation of
-//! it.
+//! This module defines abstraction over `Jieba`, and an implementation of it.
 
 /// Jieba-like types, defined so that this crate won't need to actually depend
 /// on `jieba-rs`.
@@ -23,83 +22,87 @@ pub trait JiebaPlaceholder {
 }
 
 #[cfg(test)]
-use trie_rs::Trie;
+mod keyword_cutter {
+    use trie_rs::Trie;
 
-/// Cut words deterministically according to a predefined dictionary.
-#[cfg(test)]
-pub struct KeywordCutter {
-    dict: Trie<u8>,
-}
+    use super::JiebaPlaceholder;
 
-#[cfg(test)]
-impl KeywordCutter {
-    pub fn new(dict: impl IntoIterator<Item = String>) -> Self {
-        Self {
-            dict: dict.into_iter().collect(),
+    /// Cut words deterministically according to a predefined dictionary.
+    pub struct KeywordCutter {
+        dict: Trie<u8>,
+    }
+
+    impl KeywordCutter {
+        pub fn new(dict: impl IntoIterator<Item = String>) -> Self {
+            Self {
+                dict: dict.into_iter().collect(),
+            }
         }
     }
-}
 
-#[cfg(test)]
-impl JiebaPlaceholder for KeywordCutter {
-    fn cut_hmm<'a>(&self, sentence: &'a str) -> Vec<&'a str> {
-        // A `matched_len` basically means that at the i-th char position,
-        // either sentence[i..i + matched_len[i]] is a keyword of `self`, or
-        // `matched_len[i]` is zero.
-        let matched_len: Vec<_> = sentence
-            .char_indices()
-            .map(|(start, ch)| {
-                self.dict
-                    .common_prefix_search(&sentence[start..])
-                    .map(|n: String| (n.chars().count(), n.len()))
-                    .max_by_key(|(n_chars, _)| *n_chars)
-                    .unwrap_or((0, ch.len_utf8()))
-            })
-            .collect();
+    impl JiebaPlaceholder for KeywordCutter {
+        fn cut_hmm<'a>(&self, sentence: &'a str) -> Vec<&'a str> {
+            // A `matched_len` basically means that at the i-th char position,
+            // either sentence[i..i + matched_len[i]] is a keyword of `self`,
+            // or `matched_len[i]` is zero.
+            let matched_len: Vec<_> = sentence
+                .char_indices()
+                .map(|(start, ch)| {
+                    self.dict
+                        .common_prefix_search(&sentence[start..])
+                        .map(|n: String| (n.chars().count(), n.len()))
+                        .max_by_key(|(n_chars, _)| *n_chars)
+                        .unwrap_or((0, ch.len_utf8()))
+                })
+                .collect();
 
-        /// Basically, what it does is to scan from left to right in
-        /// `matched_len`. On nonzero value n, push n to the result, and skip
-        /// the next n-1 element in `matched_len`; on zero value, find the next
-        /// nonzero value and push the number of contiguous zero values before
-        /// one is found. Finally, return the result Vec. It's guaranteed that
-        /// `matched_len` is long enough.
-        fn fold_to_bytes(matched_len: Vec<(usize, usize)>) -> Vec<usize> {
-            let mut result = Vec::new();
-            let mut i = 0;
+            /// Basically, what it does is to scan from left to right in
+            /// `matched_len`. On nonzero value n, push n to the result, and
+            /// skip the next n-1 element in `matched_len`; on zero value, find
+            /// the next nonzero value and push the number of contiguous zero
+            /// values before one is found. Finally, return the result Vec.
+            /// It's guaranteed that `matched_len` is long enough.
+            fn fold_to_bytes(matched_len: Vec<(usize, usize)>) -> Vec<usize> {
+                let mut result = Vec::new();
+                let mut i = 0;
 
-            while i < matched_len.len() {
-                let (n_chars, n_bytes) = matched_len[i];
-                if n_chars > 0 {
-                    result.push(n_bytes);
-                    i += n_chars; // Skip the next n-1 elements
-                } else {
-                    let zero_byte_count = matched_len[i..]
-                        .iter()
-                        .take_while(|(n_chars, _)| n_chars == &0)
-                        .map(|(_, n_bytes)| {
-                            i += 1;
-                            n_bytes
-                        })
-                        .sum();
-                    result.push(zero_byte_count);
+                while i < matched_len.len() {
+                    let (n_chars, n_bytes) = matched_len[i];
+                    if n_chars > 0 {
+                        result.push(n_bytes);
+                        i += n_chars; // Skip the next n-1 elements
+                    } else {
+                        let zero_byte_count = matched_len[i..]
+                            .iter()
+                            .take_while(|(n_chars, _)| n_chars == &0)
+                            .map(|(_, n_bytes)| {
+                                i += 1;
+                                n_bytes
+                            })
+                            .sum();
+                        result.push(zero_byte_count);
+                    }
                 }
+
+                result
             }
 
-            result
+            let folded = fold_to_bytes(matched_len);
+            let mut start = 0;
+            folded
+                .into_iter()
+                .map(|n_bytes| {
+                    let segment = &sentence[start..start + n_bytes];
+                    start += n_bytes;
+                    segment
+                })
+                .collect()
         }
-
-        let folded = fold_to_bytes(matched_len);
-        let mut start = 0;
-        folded
-            .into_iter()
-            .map(|n_bytes| {
-                let segment = &sentence[start..start + n_bytes];
-                start += n_bytes;
-                segment
-            })
-            .collect()
     }
 }
+
+#[cfg(test)]
+pub use keyword_cutter::KeywordCutter;
 
 #[cfg(test)]
 mod tests {
