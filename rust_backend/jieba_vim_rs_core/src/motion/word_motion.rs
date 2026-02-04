12a0c4222b2a624d2c4a93158643d320d0252cf0
kkew3 <kps6326@hotmail.com>
2026-02-04 17:44:01 +0800

    Add [nxo]map word motions prototype
    Note that this impl does not aim to be perfectly correct.

--- rust_backend/jieba_vim_rs_core/src/motion/word_motion.rs
+++ rust_backend/jieba_vim_rs_core/src/motion/word_motion.rs
@@ -19,8 +19,8 @@ pub struct WordMotion<C> {
     pub(super) tokenizer: Tokenizer<C>,
 }
 
-pub type Position = [u64; 4];
-pub type CursorPosition = [u64; 5];
+pub type Position = [usize; 4];
+pub type CursorPosition = [usize; 5];
 
 pub struct NmapOutput {
     pub cursor: CursorPosition,
@@ -49,6 +49,10 @@ impl<C> WordMotion<C> {
     }
 }
 
+fn get_motion_bytes(motion: &str) -> &[u8] {
+    &motion.as_bytes()[..2.min(motion.len())]
+}
+
 impl<C: JiebaPlaceholder> WordMotion<C> {
     pub fn nmap<B: BufferLike + ?Sized>(
         &mut self,
@@ -57,7 +61,51 @@ impl<C: JiebaPlaceholder> WordMotion<C> {
         cursor: CursorPosition,
         count: u64,
     ) -> Result<NmapOutput, B::Error> {
-        todo!()
+        let [_, lnum, col, _, _] = cursor;
+        let col_m1 = col - 1;
+        let motion_bytes = get_motion_bytes(motion);
+        let (lnum, col_m1) = match motion_bytes {
+            b"w" | b"W" => {
+                self.nmap_w(
+                    buffer,
+                    (lnum, col_m1),
+                    count,
+                    motion_bytes[0] == b'w',
+                )?
+                .new_cursor_pos
+            }
+            b"b" | b"B" => {
+                self.nmap_b(
+                    buffer,
+                    (lnum, col_m1),
+                    count,
+                    motion_bytes[0] == b'b',
+                )?
+                .new_cursor_pos
+            }
+            b"e" | b"E" => {
+                self.nmap_e(
+                    buffer,
+                    (lnum, col_m1),
+                    count,
+                    motion_bytes[0] == b'e',
+                )?
+                .new_cursor_pos
+            }
+            b"ge" | b"gE" => {
+                self.nmap_ge(
+                    buffer,
+                    (lnum, col_m1),
+                    count,
+                    motion_bytes[1] == b'e',
+                )?
+                .new_cursor_pos
+            }
+            _ => unreachable!("invalid motion key sequence: {}", motion),
+        };
+        Ok(NmapOutput {
+            cursor: [0, lnum, col_m1 + 1, 0, col_m1 + 1],
+        })
     }
 
     pub fn xmap<B: BufferLike + ?Sized>(
@@ -69,7 +117,53 @@ impl<C: JiebaPlaceholder> WordMotion<C> {
         visual_end: Position,
         count: u64,
     ) -> Result<XmapOutput, B::Error> {
-        todo!()
+        let [_, ve_lnum, ve_col, _] = visual_end;
+        let ve_col_m1 = ve_col - 1;
+        let motion_bytes = get_motion_bytes(motion);
+        let (ve_lnum, ve_col_m1) = match motion_bytes {
+            b"w" | b"W" => {
+                self.xmap_w(
+                    buffer,
+                    (ve_lnum, ve_col_m1),
+                    count,
+                    motion_bytes[0] == b'w',
+                )?
+                .new_cursor_pos
+            }
+            b"b" | b"B" => {
+                self.xmap_b(
+                    buffer,
+                    (ve_lnum, ve_col_m1),
+                    count,
+                    motion_bytes[0] == b'b',
+                )?
+                .new_cursor_pos
+            }
+            b"e" | b"E" => {
+                self.xmap_e(
+                    buffer,
+                    (ve_lnum, ve_col_m1),
+                    count,
+                    motion_bytes[0] == b'e',
+                )?
+                .new_cursor_pos
+            }
+            b"ge" | b"gE" => {
+                self.xmap_ge(
+                    buffer,
+                    (ve_lnum, ve_col_m1),
+                    count,
+                    motion_bytes[1] == b'e',
+                )?
+                .new_cursor_pos
+            }
+            _ => unreachable!("invalid motion key sequence: {}", motion),
+        };
+        Ok(XmapOutput {
+            langle: visual_begin,
+            rangle: [0, ve_lnum, ve_col_m1 + 1, 0],
+            visualmode: visualmode.into(),
+        })
     }
 
     pub fn omap<B: BufferLike + ?Sized>(
@@ -80,6 +174,88 @@ impl<C: JiebaPlaceholder> WordMotion<C> {
         count: u64,
         operator: &str,
     ) -> Result<OmapOutput, B::Error> {
-        todo!()
+        let [_, lnum, col, _, _] = cursor;
+        let col_m1 = col - 1;
+        let motion_bytes = get_motion_bytes(motion);
+        let operator_bytes = &operator.as_bytes()[..1];
+        let output = match (motion_bytes, operator_bytes) {
+            (b"w", b"c") | (b"W", b"c") => self.omap_c_w(
+                buffer,
+                (lnum, col_m1),
+                count,
+                motion_bytes[0] == b'w',
+            )?,
+            (b"w", _) | (b"W", _) => self.omap_w(
+                buffer,
+                (lnum, col_m1),
+                count,
+                motion_bytes[0] == b'w',
+            )?,
+            (b"b", _) | (b"B", _) => self.omap_b(
+                buffer,
+                (lnum, col_m1),
+                count,
+                motion_bytes[0] == b'b',
+            )?,
+            (b"e", b"d") | (b"E", b"d") => self.omap_d_e(
+                buffer,
+                (lnum, col_m1),
+                count,
+                motion_bytes[0] == b'e',
+            )?,
+            (b"e", _) | (b"E", _) => self.omap_e(
+                buffer,
+                (lnum, col_m1),
+                count,
+                motion_bytes[0] == b'e',
+            )?,
+            (b"ge", b"d") | (b"gE", b"d") => self.omap_d_ge(
+                buffer,
+                (lnum, col_m1),
+                count,
+                motion_bytes[1] == b'e',
+            )?,
+            (b"ge", _) | (b"gE", _) => self.omap_ge(
+                buffer,
+                (lnum, col_m1),
+                count,
+                motion_bytes[1] == b'e',
+            )?,
+            _ => unreachable!("invalid motion key sequence: {}", motion),
+        };
+        let (
+            next_lnum,
+            next_col_m1,
+            langle_lnum,
+            langle_col_m1,
+            rangle_lnum,
+            rangle_col_m1,
+        ) = match motion_bytes {
+            b"w" | b"W" | b"e" | b"E" => (
+                lnum,
+                col_m1,
+                lnum,
+                col_m1,
+                output.new_cursor_pos.0,
+                output.new_cursor_pos.1,
+            ),
+            b"b" | b"B" | b"ge" | b"gE" => (
+                output.new_cursor_pos.0,
+                output.new_cursor_pos.1,
+                lnum,
+                col_m1,
+                output.new_cursor_pos.0,
+                output.new_cursor_pos.1,
+            ),
+            _ => unreachable!("invalid motion key sequence: {}", motion),
+        };
+        let prevent_change =
+            if output.prevent_change { "1" } else { "0" }.into();
+        Ok(OmapOutput {
+            cursor: [0, next_lnum, next_col_m1 + 1, 0, next_col_m1 + 1],
+            langle: [0, langle_lnum, langle_col_m1 + 1, 0],
+            rangle: [0, rangle_lnum, rangle_col_m1 + 1, 0],
+            prevent_change,
+        })
     }
 }
