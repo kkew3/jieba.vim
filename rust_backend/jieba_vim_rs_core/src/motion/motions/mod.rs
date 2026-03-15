6bdc9bdd00ff795b12dc41460b7860b7bae62d6a
kkew3 <kps6326@hotmail.com>
2026-03-15 17:43:44 +0800

    Add `motion::motions` mod

--- rust_backend/jieba_vim_rs_core/src/motion/motions/mod.rs
+++ rust_backend/jieba_vim_rs_core/src/motion/motions/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2024-2026 Kaiwen Wu. All Rights Reserved.
+// Copyright 2026 Kaiwen Wu. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy
@@ -12,22 +12,4 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-mod api;
-pub(crate) mod core;
-pub(crate) mod motions;
-mod nmap_b;
-mod nmap_e;
-mod nmap_ge;
-mod nmap_w;
-mod omap_b;
-mod omap_e;
-mod omap_ge;
-mod omap_w;
-pub(crate) mod policy;
-mod xmap_b;
-mod xmap_e;
-mod xmap_ge;
-mod xmap_w;
-
-pub use api::WordMotion;
-pub use api::ffi::{NmapOutput, OmapOutput, XmapOutput};
+//! The motions.
