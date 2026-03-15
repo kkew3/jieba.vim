45f5efbc4d824dc2ac4b7a1c37437cc835baa324
kkew3 <kps6326@hotmail.com>
2026-03-15 17:44:44 +0800

    Add `text_object` mod and attach Vim's license text

--- rust_backend/jieba_vim_rs_core/src/motion/motions/text_object.rs
+++ rust_backend/jieba_vim_rs_core/src/motion/motions/text_object.rs
@@ -1,4 +1,13 @@
 // Copyright 2026 Kaiwen Wu. All Rights Reserved.
+// Portions Copyright (c) by Bram Moolenaar and others.
+//
+// This file contains code adapted from Vim's textobject.c. The Vim License
+// applies to the adapted portions. See the vim-LICENSE.txt file in the project
+// root for the full license text.
+//
+// In accordance with the Vim License (Section II):
+// - Contact: Kaiwen Wu <kps6326@hotmail.com>
+// - Changes are available to the Vim maintainer upon request.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy
@@ -12,6 +21,7 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-//! The motions.
-
-pub mod text_object;
+//! Text objects.
+//!
+//! Note that the term "text objects" is quoted from Bram's C code. It refers
+//! to all atomic word motions underneath |w|, |b|, |e|, |ge|, |iw|, |aw|.
