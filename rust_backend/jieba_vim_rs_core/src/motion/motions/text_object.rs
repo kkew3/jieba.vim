8b1644ead8db8041bfc642be4f07fbe398133de5
kkew3 <kps6326@hotmail.com>
2026-03-16 19:08:39 +0800

    Add `BackwardWord` primitive motion

--- rust_backend/jieba_vim_rs_core/src/motion/motions/text_object.rs
+++ rust_backend/jieba_vim_rs_core/src/motion/motions/text_object.rs
@@ -29,7 +29,7 @@
 use crate::token::{Token, TokenLike, TokenType};
 
 use super::core::buffer::ParsedBufferLike;
-use super::core::failure::{AbsolutelyIntolerable, SemiTolerable};
+use super::core::failure::{AbsolutelyIntolerable, SemiTolerable, Tolerable};
 use super::core::iter::{ExtendedInlineTokensIter, GToken, TokenLikeExt};
 use super::core::motion::{
     ExtendedMotionState, FoldState, Markovian, MarkovianUnit, Motion,
@@ -422,5 +422,153 @@ mod end_word {
     }
 }
 
+mod bck_word {
+    use super::*;
+
+    /// Move backward `count` words.
+    pub struct BackwardWord {
+        /// True to move one less if we are already on the start of a word.
+        stop: bool,
+    }
+
+    impl BackwardWord {
+        /// Construct a new [`BackwardWord`]. Pass true to `stop` to move one
+        /// less if we are already on the start of a word.
+        pub fn new(stop: bool) -> Self {
+            Self { stop }
+        }
+    }
+
+    impl Motion<Position> for BackwardWord {
+        fn map<B: ParsedBufferLike + ?Sized>(
+            &mut self,
+            buffer: &mut B,
+            count: u64,
+            cursor: &mut Position,
+        ) -> Result<MotionState, B::Error> {
+            let mut motion =
+                Markovian::new(UnitBackwardWord { stop: self.stop });
+            motion.map(buffer, count, cursor)
+        }
+    }
+
+    struct UnitBackwardWord {
+        /// True to not move at all if we are already on the start of a word.
+        stop: bool,
+    }
+
+    impl UnitMotion<Position> for UnitBackwardWord {
+        fn unit_map<B: ParsedBufferLike + ?Sized>(
+            &mut self,
+            buffer: &mut B,
+            cursor: &mut Position,
+        ) -> Result<ExtendedMotionState, B::Error> {
+            let stop = self.stop;
+            self.stop = false;
+
+            let Position { lnum, col, off } = cursor;
+            *off = 0;
+
+            // Quick path.
+            if *lnum == 1 && *col == 1 {
+                return Ok(ExtendedMotionState::Failure);
+            }
+
+            let tokens = buffer.getline_parsed(*lnum)?;
+            let mut line = ExtendedInlineTokensIter::new(&tokens)
+                .take_col_rev(*col)
+                .expect("col too large")
+                .peekable();
+            // `unwrap` is safe because `take_col_rev` yields at least one
+            // item.
+            let cursor_token = line.next().unwrap();
+
+            if *lnum == 1 && line.peek().is_none() {
+                let s = match &cursor_token {
+                    // cursor_token can't be Eol, since it would result in col
+                    // == 1, but we have tested for col == 1 above.
+                    GToken::Eol(_) => unreachable!(),
+                    GToken::T(t) => {
+                        // If we are at a regular token at bof ..
+
+                        // We have tested that col > 1 above.
+                        *col = 1;
+                        match t.ty {
+                            TokenType::Space => ExtendedMotionState::Pending,
+                            TokenType::Word => ExtendedMotionState::Success,
+                        }
+                    }
+                };
+                return Ok(s);
+            }
+
+            if let GToken::T(t) = &cursor_token
+                && t.ty == TokenType::Word
+            {
+                if !t.at_start(*col) {
+                    *col = t.first_char();
+                    return Ok(ExtendedMotionState::Success);
+                }
+                if stop {
+                    return Ok(ExtendedMotionState::Success);
+                }
+            }
+
+            let s = match find_stop_point(line, col) {
+                // `unwrap` is safe because `find_stop_point` return only empty
+                // line or words.
+                Some(t) => ExtendedMotionState::from_dest_token(t).unwrap(),
+                None => loop {
+                    // `line` can't be empty when `lnum` == 1, as we have
+                    // covered above.
+                    if *lnum <= 1 {
+                        // Calling |b| on a Space at bof ..
+                        break ExtendedMotionState::Pending;
+                    }
+                    *lnum -= 1;
+                    let tokens = buffer.getline_parsed(*lnum)?;
+                    let line = ExtendedInlineTokensIter::new(&tokens).rev();
+                    if let Some(t) = find_stop_point(line, col) {
+                        // `unwrap` is safe because `find_stop_point` return
+                        // only empty line or words.
+                        break ExtendedMotionState::from_dest_token(t).unwrap();
+                    }
+                },
+            };
+            Ok(s)
+        }
+    }
+
+    impl MarkovianUnit<Position> for UnitBackwardWord {
+        type FoldState = Tolerable;
+    }
+
+    /// Test if a token is stoppable.
+    fn is_stoppable(token: &GToken) -> bool {
+        match token {
+            GToken::Eol(1) => true,
+            GToken::Eol(_) => false,
+            GToken::T(token) => match token.ty {
+                TokenType::Word => true,
+                TokenType::Space => false,
+            },
+        }
+    }
+
+    fn find_stop_point<L: IntoIterator<Item = GToken>>(
+        line: L,
+        col: &mut usize,
+    ) -> Option<GToken> {
+        for token in line {
+            *col = token.first_char();
+            if is_stoppable(&token) {
+                return Some(token);
+            }
+        }
+        None
+    }
+}
+
+pub use bck_word::BackwardWord;
 pub use end_word::EndWord;
 pub use fwd_word::ForwardWord;
