777724a94e14d293aadcb1ea69c7f192d3d7843d
kkew3 <kps6326@hotmail.com>
2026-03-16 11:37:36 +0800

    Add `EndWord` primitive motion

--- rust_backend/jieba_vim_rs_core/src/motion/motions/text_object.rs
+++ rust_backend/jieba_vim_rs_core/src/motion/motions/text_object.rs
@@ -26,10 +26,10 @@
 //! Note that the term "text objects" is quoted from Bram's C code. It refers
 //! to all atomic word motions underneath |w|, |b|, |e|, |ge|, |iw|, |aw|.
 
-use crate::token::{TokenLike, TokenType};
+use crate::token::{Token, TokenLike, TokenType};
 
 use super::core::buffer::ParsedBufferLike;
-use super::core::failure::SemiTolerable;
+use super::core::failure::{AbsolutelyIntolerable, SemiTolerable};
 use super::core::iter::{ExtendedInlineTokensIter, GToken, TokenLikeExt};
 use super::core::motion::{
     ExtendedMotionState, FoldState, Motion, MotionState, UnitMotion,
@@ -205,4 +205,225 @@ mod fwd_word {
     }
 }
 
+mod end_word {
+    use super::*;
+
+    /// Move to the end of `count` words.
+    pub struct EndWord {
+        /// True to move one less if we are already on the end of a word.
+        stop: bool,
+        /// True to stop on empty lines.
+        empty: bool,
+    }
+
+    impl EndWord {
+        /// Construct a new [`EndWord`]. Pass true to `stop` to move one less
+        /// if we are already on the end of a word. Pass true to `empty` to
+        /// stop on empty lines.
+        pub fn new(stop: bool, empty: bool) -> Self {
+            Self { stop, empty }
+        }
+    }
+
+    impl Motion<Position> for EndWord {
+        fn map<B: ParsedBufferLike + ?Sized>(
+            &mut self,
+            buffer: &mut B,
+            mut count: u64,
+            cursor: &mut Position,
+        ) -> Result<MotionState, B::Error> {
+            let mut state = AbsolutelyIntolerable::default();
+            let orig_count = count;
+            while count > 0 {
+                let mut unit_motion = UnitEndWord {
+                    stop: self.stop && count == orig_count,
+                    empty: self.empty,
+                };
+                if let Some(absorbing_state) =
+                    state.update(unit_motion.unit_map(buffer, cursor)?)
+                {
+                    return Ok(absorbing_state);
+                }
+                count -= 1;
+            }
+            Ok(state.finalize())
+        }
+    }
+
+    /// Note that this is a [`UnitMotion`], but not a
+    /// [`MarkovianUnit`](crate::motion::core::motion::MarkovianUnit).
+    struct UnitEndWord {
+        /// True to not move at all if we are already on the end of a word.
+        stop: bool,
+        /// True to stop on empty lines.
+        empty: bool,
+    }
+
+    impl UnitMotion<Position> for UnitEndWord {
+        fn unit_map<B: ParsedBufferLike + ?Sized>(
+            &mut self,
+            buffer: &mut B,
+            cursor: &mut Position,
+        ) -> Result<ExtendedMotionState, B::Error> {
+            let Position { lnum, col, off } = cursor;
+            *off = 0;
+
+            let n_lines = buffer.lines()?;
+            let tokens = buffer.getline_parsed(*lnum)?;
+            let mut line = ExtendedInlineTokensIter::new(&tokens)
+                .skip_col(*col)
+                .expect("col too large")
+                .peekable();
+            let cursor_token = line.next().unwrap();
+
+            /// Handle the case where `cursor_token` is the last regular token
+            /// before Eol(_) at eof.
+            fn last_line_eof_case(
+                cursor_token: &Token,
+                col: &mut usize,
+                stop: bool,
+            ) -> ExtendedMotionState {
+                match cursor_token.ty {
+                    TokenType::Word => {
+                        if cursor_token.at_end(*col) {
+                            if stop {
+                                ExtendedMotionState::Success
+                            } else {
+                                *col = cursor_token.last_char1();
+                                ExtendedMotionState::Failure
+                            }
+                        } else {
+                            *col = cursor_token.last_char();
+                            ExtendedMotionState::Success
+                        }
+                    }
+                    TokenType::Space => {
+                        *col = cursor_token.last_char1();
+                        ExtendedMotionState::Pending
+                    }
+                }
+            }
+
+            if *lnum == n_lines {
+                match line.peek() {
+                    None => {
+                        assert!(cursor_token.is_empty());
+                        return Ok(ExtendedMotionState::Failure);
+                    }
+                    Some(next_t) => {
+                        if next_t.is_empty() {
+                            let s = match &cursor_token {
+                                GToken::T(cursor_token) => last_line_eof_case(
+                                    cursor_token,
+                                    col,
+                                    self.stop,
+                                ),
+                                // If `next_t` exists, `cursor_token` can't be
+                                // empty.
+                                GToken::Eol(_) => unreachable!(),
+                            };
+                            return Ok(s);
+                        }
+                    }
+                }
+            }
+
+            if let GToken::T(t) = &cursor_token
+                && t.ty == TokenType::Word
+            {
+                if !t.at_end(*col) {
+                    *col = t.last_char();
+                    return Ok(ExtendedMotionState::Success);
+                } else if self.stop {
+                    return Ok(ExtendedMotionState::Success);
+                }
+            }
+
+            fn is_not_non_empty_line_eol(token: &GToken) -> bool {
+                match token {
+                    GToken::T(_) => true,
+                    GToken::Eol(1) => true,
+                    GToken::Eol(_) => false,
+                }
+            }
+
+            // We need to filter out non-empty line Eol because they occur
+            // after all other tokens in a line, and thus prevents us from
+            // collecting the last regular token (or empty line Eol(1)) of
+            // each line.
+            let line = line.filter(is_not_non_empty_line_eol);
+            let s = match find_stop_point(line, col, self.empty) {
+                Ok(GToken::T(_)) => ExtendedMotionState::Success,
+                // This branch will be reached only if `self.empty` is true.
+                Ok(GToken::Eol(_)) => ExtendedMotionState::SemiFailure,
+                Err(mut last_t) => loop {
+                    if *lnum >= n_lines {
+                        // If `lnum` == `n_lines`, `last_t` can't be None; and
+                        // if `last_t` is None, `lnum` can't be `n_lines`.
+                        let s = match last_t.unwrap() {
+                            GToken::T(t) => match t.ty {
+                                // If `last_t` were a word, it should already
+                                // be captured by `find_stop_point`.
+                                TokenType::Word => unreachable!(),
+                                TokenType::Space => {
+                                    *col = t.last_char1();
+                                    ExtendedMotionState::Pending
+                                }
+                            },
+                            GToken::Eol(1) => ExtendedMotionState::SemiFailure,
+                            // Due to the filtering.
+                            GToken::Eol(_) => unreachable!(),
+                        };
+                        break s;
+                    }
+                    *lnum += 1;
+                    let tokens = buffer.getline_parsed(*lnum)?;
+                    let line = ExtendedInlineTokensIter::new(&tokens)
+                        .filter(is_not_non_empty_line_eol);
+                    match find_stop_point(line, col, self.empty) {
+                        Ok(GToken::T(_)) => break ExtendedMotionState::Success,
+                        // This branch will be reached only if `self.empty`
+                        // is true.
+                        Ok(GToken::Eol(_)) => {
+                            break ExtendedMotionState::SemiFailure;
+                        }
+                        Err(curr_last_t) => last_t = curr_last_t,
+                    }
+                },
+            };
+            Ok(s)
+        }
+    }
+
+    /// Return either the stop point (a Word), or the last token yielded by
+    /// `line`. When `empty` is true, the stop point may also be an Eol(1).
+    fn find_stop_point<L: IntoIterator<Item = GToken>>(
+        line: L,
+        col: &mut usize,
+        empty: bool,
+    ) -> Result<GToken, Option<GToken>> {
+        let mut last_token = None;
+        for token in line {
+            last_token = Some(token);
+            match token {
+                GToken::T(t) => {
+                    *col = t.last_char();
+                    if t.ty == TokenType::Word {
+                        return Ok(token);
+                    }
+                }
+                GToken::Eol(1) => {
+                    *col = 1;
+                    if empty {
+                        return Ok(token);
+                    }
+                }
+                GToken::Eol(_) => (),
+            }
+        }
+        Err(last_token)
+    }
+}
+
+pub use end_word::EndWord;
 pub use fwd_word::ForwardWord;
