bf2858563e377953b17544c025f542993ef18729
kkew3 <kps6326@hotmail.com>
2026-03-18 17:15:52 +0800

    Remove dead code

--- rust_backend/jieba_vim_rs_core/src/motion/nmap_ge.rs
+++ rust_backend/jieba_vim_rs_core/src/motion/nmap_ge.rs
@@ -13,15 +13,11 @@
 // under the License.
 
 use crate::BufferLike;
-use crate::token::{JiebaPlaceholder, TokenLike, TokenType};
+use crate::token::JiebaPlaceholder;
 
 use super::api::{NmapOutput, WordMotion};
-use super::core::buffer::{ParsedBuffer, ParsedBufferLike};
-use super::core::failure::SemiTolerable;
-use super::core::iter::{ExtendedInlineTokensIter, GToken};
-use super::core::motion::{
-    ExtendedMotionState, MarkovianUnit, Motion, UnitMotion,
-};
+use super::core::buffer::ParsedBuffer;
+use super::core::motion::Motion;
 use super::core::position::Position;
 use super::primitives::text_object::BackwardEndWord;
 
@@ -58,96 +54,4 @@ impl<C: JiebaPlaceholder> WordMotion<C> {
             prevent_change: s.into_prevent_change(),
         })
     }
-}
-
-pub struct UnitNmapGe;
-
-impl UnitMotion<Position> for UnitNmapGe {
-    fn unit_map<B: ParsedBufferLike + ?Sized>(
-        &mut self,
-        buffer: &mut B,
-        cursor: &mut Position,
-    ) -> Result<ExtendedMotionState, B::Error> {
-        let Position { lnum, col, off } = cursor;
-        *off = 0;
-
-        // Quick path.
-        if *lnum == 1 && *col == 1 {
-            return Ok(ExtendedMotionState::Failure);
-        }
-
-        let tokens = buffer.getline_parsed(*lnum)?;
-        let mut line = ExtendedInlineTokensIter::new(&tokens)
-            .take_col_rev(*col)
-            .expect("col too large")
-            .peekable();
-        let cursor_token = line.next().unwrap();
-
-        if *lnum == 1 && line.peek().is_none() {
-            let s = match &cursor_token {
-                // cursor_token can't be Eol, since it would result in col ==
-                // 1, but we have tested for col == 1 above.
-                GToken::Eol(_) => unreachable!(),
-                GToken::T(t) => {
-                    // If we are at a regular token at bof ..
-
-                    // We have tested that col > 1 above.
-                    *col = 1;
-                    match t.ty {
-                        TokenType::Space => ExtendedMotionState::Pending,
-                        TokenType::Word => ExtendedMotionState::Success,
-                    }
-                }
-            };
-            return Ok(s);
-        }
-
-        let s = match find_stop_point(line, col) {
-            // `unwrap` is safe because `find_stop_point` return only empty
-            // line or words.
-            Some(t) => ExtendedMotionState::from_dest_token(t).unwrap(),
-            None => loop {
-                // `line` can't be empty when `lnum` == 1, as we have covered
-                // above.
-                if *lnum <= 1 {
-                    // Calling |ge| on a Space at bof ..
-                    break ExtendedMotionState::Pending;
-                }
-                *lnum -= 1;
-                let tokens = buffer.getline_parsed(*lnum)?;
-                let line = ExtendedInlineTokensIter::new(&tokens).rev();
-                if let Some(t) = find_stop_point(line, col) {
-                    break ExtendedMotionState::from_dest_token(t).unwrap();
-                }
-            },
-        };
-        Ok(s)
-    }
-}
-
-impl MarkovianUnit<Position> for UnitNmapGe {
-    type FoldState = SemiTolerable;
-}
-
-fn find_stop_point<L: IntoIterator<Item = GToken>>(
-    line: L,
-    col: &mut usize,
-) -> Option<GToken> {
-    for token in line {
-        match token {
-            GToken::Eol(1) => {
-                *col = token.first_char();
-                return Some(token);
-            }
-            GToken::Eol(_) => (),
-            GToken::T(t) => match t.ty {
-                TokenType::Space => *col = t.first_char(),
-                TokenType::Word => {
-                    *col = t.last_char();
-                    return Some(token);
-                }
-            },
-        }
-    }
-    None
 }
--- rust_backend/jieba_vim_rs_core/src/motion/policy/d_special.rs
+++ rust_backend/jieba_vim_rs_core/src/motion/policy/d_special.rs
@@ -27,7 +27,7 @@ use crate::token::TokenType;
 
 use super::core::buffer::ParsedBufferLike;
 use super::core::iter::{ExtendedInlineTokensIter, GToken, TokenLikeExt};
-use super::core::position::{OperatorRange, Position};
+use super::core::position::OperatorRange;
 use super::primitives::predicate::OnOrBeforeFirstNonBlanks;
 
 /// Check if current motion satisfies d-special case, and make the motion
@@ -95,100 +95,5 @@ impl<'o> DSpecial for OperatorRange<'o> {
         // Make the motion linewise if it's indeed d-special.
         self.mtype = MotionType::LineInclusive;
         Ok(())
-    }
-}
-
-/// Check if current motion satisfies d-special case. See
-/// https://vimhelp.org/change.txt.html#d-special.
-pub fn is_d_special<B: ParsedBufferLike + ?Sized>(
-    buffer: &mut B,
-    langle: Position,
-    rangle: Position,
-    inclusive: bool,
-) -> Result<bool, B::Error> {
-    let (langle, rangle) = if langle <= rangle {
-        (langle, rangle)
-    } else {
-        (rangle, langle)
-    };
-    let Position {
-        lnum: llnum,
-        col: lcol,
-        ..
-    } = langle;
-    let Position {
-        lnum: rlnum,
-        col: rcol,
-        ..
-    } = rangle;
-    if rcol == 1 && !inclusive {
-        panic!(
-            "`exclusive + rcol=1` case must be handled first by \
-            `exclusive_special` mod"
-        );
-    }
-    if llnum == rlnum {
-        return Ok(false);
-    }
-
-    let lline = buffer.getline_parsed(llnum)?;
-    let mut ltokens = ExtendedInlineTokensIter::new(&lline)
-        .take_col_rev(lcol)
-        .expect("col of langle too large");
-    if let GToken::T(t) = ltokens.next().unwrap()
-        && t.ty == TokenType::Word
-        && !t.at_start(lcol)
-    {
-        return Ok(false);
-    }
-    for token in ltokens {
-        if let GToken::T(t) = token
-            && t.ty == TokenType::Word
-        {
-            return Ok(false);
-        }
-    }
-
-    let rline = buffer.getline_parsed(rlnum)?;
-    let mut rtokens = ExtendedInlineTokensIter::new(&rline)
-        .skip_col(rcol)
-        .expect("col of rangle too large");
-    if let GToken::T(t) = rtokens.next().unwrap()
-        && t.ty == TokenType::Word
-    {
-        if !inclusive {
-            return Ok(false);
-        }
-        if !t.at_end(rcol) {
-            return Ok(false);
-        }
-    }
-    for token in rtokens {
-        if let GToken::T(t) = token
-            && t.ty == TokenType::Word
-        {
-            return Ok(false);
-        }
-    }
-
-    Ok(true)
-}
-
-/// `cursor` is essentially arbitrary for d-special; setting `cursor` to
-/// (lnum=1, col=1, off=0) to please the verifier, in case d-special deletes
-/// the entire buffer.
-pub fn reset_cursor_when_d_special(
-    n_lines: usize,
-    langle: &Position,
-    rangle: &Position,
-    cursor: &mut Position,
-) {
-    let llnum = langle.lnum;
-    let rlnum = rangle.lnum;
-    let Position { lnum, col, off } = cursor;
-    if llnum == 1 && rlnum == n_lines {
-        *lnum = 1;
-        *col = 1;
-        *off = 0;
     }
 }
