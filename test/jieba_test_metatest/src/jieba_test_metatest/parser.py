c2943b16cbef48e7c6a77fe66baf2cb7cbcdc1a4
kkew3 <kps6326@hotmail.com>
2026-05-15 19:52:03 +0800

    Add several expression parser and tests

--- test/jieba_test_metatest/src/jieba_test_metatest/parser.py
+++ test/jieba_test_metatest/src/jieba_test_metatest/parser.py
@@ -12,10 +12,8 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-"""Basic parsing."""
-
 from dataclasses import dataclass
-from typing import Iterable, Literal
+from typing import Iterable, Iterator, Literal
 
 
 @dataclass
@@ -121,6 +119,12 @@ class RawBlock:
         """Stable sort `self` by directive types."""
         self.directives.sort(key=lambda dr: dr.ty)
 
+    def iter_directives_like(
+        self, dr: RawDirective | str
+    ) -> Iterator[RawDirective]:
+        dr_type = dr if isinstance(dr, str) else dr.ty
+        return iter(dr for dr in self.directives if dr.ty == dr_type)
+
 
 @dataclass
 class RawTestCases:
@@ -142,6 +146,18 @@ class RawTestCases:
         stage: Literal["PRE_BLOCK", "INSIDE_BLOCK", "OUTSIDE_BLOCK"] = (
             "PRE_BLOCK"
         )
+        supported_defaults = {
+            "X",
+            "M",
+            "K",
+            "O",
+            "R",
+            "C",
+            "S0",
+            "B0",
+            "Q",
+            "E",
+        }
 
         for lineno, line in enumerate(lines, 1):
             line = line.rstrip("\n")
@@ -151,6 +167,7 @@ class RawTestCases:
             # Reset defaults.
             if line.startswith("##"):
                 defaults.clear()
+                continue
             line_span = span.copy_as(lineno)
 
             if stage == "PRE_BLOCK":
@@ -174,6 +191,10 @@ class RawTestCases:
                     )
                 elif ty.startswith("#"):
                     # Line is a global default.
+                    if ty[1:] not in supported_defaults:
+                        raise line_span.to_parse_error(
+                            f"unsupported directive `{ty}`"
+                        )
                     defaults.extend(
                         RawDirective(ty[1:], a, line_span.copy()) for a in args
                     )
@@ -226,6 +247,10 @@ class RawTestCases:
 
                 if ty.startswith("#"):
                     # If line is a global default ..
+                    if ty[1:] not in supported_defaults:
+                        raise line_span.to_parse_error(
+                            f"unsupported directive `{ty}`"
+                        )
                     defaults.extend(
                         RawDirective(ty[1:], a, line_span.copy()) for a in args
                     )
@@ -248,3 +273,298 @@ class RawTestCases:
         span = SourceSpan.for_file(path)
         with open(path, encoding="utf-8") as infile:
             self.extend_from_lines(infile, span)
+
+
+@dataclass
+class StateExpr:
+    ty: Literal["opt", "func", "reg", "mark"]
+    name: str
+    # Will be of type list[int] | None when ty == "mark"; otherwise, will be of
+    # type str | None.
+    value: str | list[int] | None
+
+    @classmethod
+    def parse(
+        cls,
+        arg: str,
+        span: SourceSpan,
+        parse_as_incomplete: bool = False,
+    ):
+        """
+        If `parse_as_incomplete` is passed True, the value will be ignored and
+        be set to None.
+        """
+        name, _, value = arg.partition("=")
+        if parse_as_incomplete:
+            value = None
+        if name.endswith("()"):
+            return cls("func", name[:-2], value)
+        if name.startswith('"'):
+            return cls("reg", name[1:], value)
+        if name.startswith("'"):
+            if value is not None:
+                if not value.startswith("[") or not value.endswith("]"):
+                    raise span.to_parse_error(
+                        f"mark's value should be list[int] but got: {value}"
+                    )
+                value_arr = [int(x) for x in value[1:-1].split(",")]
+                return cls("mark", name[1:], value_arr)
+            return cls("mark", name[1:], None)
+        return cls("opt", name, value)
+
+
+class Cursor:
+    def __init__(self, list_):
+        self.list_ = list_
+        self.n = len(self.list_)
+        self.j = 0
+
+    def peek(self):
+        if self.j < self.n:
+            return self.list_[self.j]
+        return None
+
+    def next(self):
+        if self.j < self.n:
+            a = self.list_[self.j]
+            self.j += 1
+            return a
+        return None
+
+
+@dataclass
+class BufferExpr:
+    clean_buffer: list[str]
+    # 4-element list
+    langle: list[int] | None
+    # 4-element list
+    rangle: list[int] | None
+    # 4-element list
+    visual_begin: list[int] | None
+    # 4-element list
+    visual_end: list[int] | None
+    # 5-element list
+    cursor: list[int] | None
+
+    @classmethod
+    def parse(cls, arg: str, span: SourceSpan):
+        position_marks = {"<", ">", "[", "]", "|", "\\"}
+        eol_marks = {"␊", "␀"}
+        markup_buffer = []
+        line = ""
+        chars = Cursor(list(arg))
+        is_empty_buffer = False
+        while (c := chars.next()) is not None:
+            line += c
+            if c in eol_marks:
+                if c == "␀":
+                    is_empty_buffer = True
+                chars_after_lf = ""
+                any_tilde = False
+                while (c1 := chars.peek()) is not None and (
+                    c1 == "~" or c1 in position_marks
+                ):
+                    chars_after_lf += chars.next()
+                    if c1 == "~":
+                        any_tilde = True
+                if any_tilde:
+                    line += chars_after_lf
+                    markup_buffer.append(line)
+                    line = ""
+                else:
+                    markup_buffer.append(line)
+                    line = chars_after_lf
+        if line or (is_empty_buffer and len(markup_buffer) > 1):
+            raise span.to_parse_error(f"invalid directive `B_` value: {arg}")
+
+        langle = None
+        rangle = None
+        visual_begin = None
+        visual_end = None
+        cursor = None
+        clean_buffer = []
+        for lineno, markup_line in enumerate(markup_buffer, 1):
+            langle_col = None
+            rangle_col = None
+            visual_begin_col = None
+            visual_end_col = None
+            cursor_col = None
+            curswant_col = None
+            clean_chars = []
+            has_virtual_cols = False
+            for i, c in enumerate(reversed(markup_line)):
+                if i == 0 and c == "~":
+                    has_virtual_cols = True
+                if c not in position_marks:
+                    clean_chars.append(c)
+                else:
+                    if not clean_chars:
+                        raise span.to_parse_error(
+                            f"invalid directive `B_` value: {arg}"
+                        )
+                    counter = len(clean_chars)
+                    if c == "<":
+                        if langle is not None or langle_col is not None:
+                            raise span.to_parse_error(
+                                f"invalid directive `B_` value: {arg}"
+                            )
+                        langle_col = counter
+                    elif c == ">":
+                        if rangle is not None or rangle_col is not None:
+                            raise span.to_parse_error(
+                                f"invalid directive `B_` value: {arg}"
+                            )
+                        rangle_col = counter
+                    elif c == "[":
+                        if (
+                            visual_begin is not None
+                            or visual_begin_col is not None
+                        ):
+                            raise span.to_parse_error(
+                                f"invalid directive `B_` value: {arg}"
+                            )
+                        visual_begin_col = counter
+                    elif c == "]":
+                        if visual_end is not None or visual_end_col is not None:
+                            raise span.to_parse_error(
+                                f"invalid directive `B_` value: {arg}"
+                            )
+                        visual_end_col = counter
+                    elif c == "|":
+                        if cursor is not None or cursor_col is not None:
+                            raise span.to_parse_error(
+                                f"invalid directive `B_` value: {arg}"
+                            )
+                        cursor_col = counter
+                    elif c == "\\":
+                        if cursor is not None or curswant_col is not None:
+                            raise span.to_parse_error(
+                                f"invalid directive `B_` value: {arg}"
+                            )
+                        curswant_col = counter
+                    else:
+                        assert False, "unreachable"
+            if cursor_col is None and curswant_col is not None:
+                raise span.to_parse_error(
+                    f"invalid directive `B_` value: {arg}"
+                )
+
+            implicit_curswant = False
+            if cursor_col is not None and curswant_col is None:
+                curswant_col = cursor_col
+                implicit_curswant = True
+
+            # Take complementation.
+            n_clean = len(clean_chars)
+            if langle_col is not None:
+                langle_col = n_clean - langle_col
+            if rangle_col is not None:
+                rangle_col = n_clean - rangle_col
+            if visual_begin_col is not None:
+                visual_begin_col = n_clean - visual_begin_col
+            if visual_end_col is not None:
+                visual_end_col = n_clean - visual_end_col
+            if cursor_col is not None:
+                cursor_col = n_clean - cursor_col
+            if curswant_col is not None:
+                curswant_col = n_clean - curswant_col
+
+            # Compute (col, off) and prune auxiliary chars.
+            col = 0
+            off = 0
+            curswant = 0
+            cursor_curswant = None
+            final_clean_chars = []
+            cursor_assigned = False
+            for i, c in enumerate(reversed(clean_chars)):
+                if col == 0 and c == "~":
+                    raise span.to_parse_error(
+                        f"invalid directive `B_` value: {arg}"
+                    )
+                if c == "~":
+                    off += 1
+                else:
+                    col += 1
+                    off = 0
+                curswant += 1
+
+                if langle_col is not None and i == langle_col:
+                    langle = [0, lineno, col, off]
+                if rangle_col is not None and i == rangle_col:
+                    rangle = [0, lineno, col, off]
+                if visual_begin_col is not None and i == visual_begin_col:
+                    visual_begin = [0, lineno, col, off]
+                if visual_end_col is not None and i == visual_end_col:
+                    visual_end = [0, lineno, col, off]
+                if cursor_col is not None and i == cursor_col:
+                    cursor = [0, lineno, col, off, 0]
+                    cursor_assigned = True
+                if curswant_col is not None and i == curswant_col:
+                    if (
+                        c in eol_marks
+                        and not implicit_curswant
+                        and not has_virtual_cols
+                    ):
+                        cursor_curswant = 2147483647  # v:maxcol
+                    else:
+                        cursor_curswant = curswant
+
+                if c == "·":
+                    final_clean_chars.append(" ")
+                elif c == "┤":
+                    final_clean_chars.append("\t")
+                elif c not in eol_marks and c not in {"@", "~"}:
+                    final_clean_chars.append(c)
+
+            if cursor_assigned and cursor is not None:
+                assert cursor_curswant is not None
+                cursor[4] = cursor_curswant
+
+            clean_buffer.append("".join(final_clean_chars))
+
+        if is_empty_buffer:
+            if any(s for s in clean_buffer):
+                raise span.to_parse_error(
+                    f"invalid directive `B_` value: {arg}"
+                )
+            clean_buffer.clear()
+
+        return cls(
+            clean_buffer,
+            langle,
+            rangle,
+            visual_begin,
+            visual_end,
+            cursor,
+        )
+
+
+@dataclass
+class AutocmdEventCountExpr:
+    name: str
+    count: int | None
+
+    @classmethod
+    def parse(
+        cls,
+        arg: str,
+        span: SourceSpan,
+        parse_as_incomplete: bool = False,
+    ):
+        """
+        If `parse_as_incomplete` is passed True, the count will be ignored and
+        be set to None.
+        """
+        name, _, value = arg.partition("=")
+        if not name:
+            raise span.to_parse_error(f"invalid directive `E` value: {arg}")
+        if parse_as_incomplete:
+            count = None
+        else:
+            try:
+                count = int(value)
+            except ValueError as err:
+                raise span.to_parse_error(
+                    f"invalid directive `E` value: {arg}"
+                ) from err
+        return cls(name, count)
--- test/jieba_test_metatest/src/jieba_test_metatest/test_parser.py
+++ test/jieba_test_metatest/src/jieba_test_metatest/test_parser.py
@@ -1,3 +1,5 @@
+import pytest
+
 from . import parser as m
 
 
@@ -111,3 +113,192 @@ S1 "a=bar
         m.RawDirective("X", "u", span.copy_as(19)),
     ]
     assert raw_test_cases.blocks[1].span == span.copy_as(18, 23)
+
+
+def test_parse_state_expr():
+    p = m.StateExpr.parse
+
+    span = m.SourceSpan()
+    assert p("visualmode()=v", span) == m.StateExpr("func", "visualmode", "v")
+    assert p("visualmode()=", span, parse_as_incomplete=True) == m.StateExpr(
+        "func", "visualmode", None
+    )
+    assert p('"a=foo', span) == m.StateExpr("reg", "a", "foo")
+    assert p('"a=', span) == m.StateExpr("reg", "a", "")
+    assert p('"a=', span, parse_as_incomplete=True) == m.StateExpr(
+        "reg", "a", None
+    )
+    assert p("'a=[0,1,2,0]", span) == m.StateExpr("mark", "a", [0, 1, 2, 0])
+    assert p("'a=", span, parse_as_incomplete=True) == m.StateExpr(
+        "mark", "a", None
+    )
+    assert p("selection=inclusive", span) == m.StateExpr(
+        "opt", "selection", "inclusive"
+    )
+
+
+def test_parse_buffer_expr():
+    span = m.SourceSpan.for_file("foo").copy_as(3)
+    assert m.BufferExpr.parse("␀", span) == m.BufferExpr(
+        clean_buffer=[],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=None,
+    )
+    assert m.BufferExpr.parse("<|>␀", span) == m.BufferExpr(
+        clean_buffer=[],
+        langle=[0, 1, 1, 0],
+        rangle=[0, 1, 1, 0],
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 1, 0, 1],
+    )
+    assert m.BufferExpr.parse("<|>\\␀", span) == m.BufferExpr(
+        clean_buffer=[],
+        langle=[0, 1, 1, 0],
+        rangle=[0, 1, 1, 0],
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 1, 0, 2147483647],
+    )
+    assert m.BufferExpr.parse("<|>\\␀~", span) == m.BufferExpr(
+        clean_buffer=[],
+        langle=[0, 1, 1, 0],
+        rangle=[0, 1, 1, 0],
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 1, 0, 1],
+    )
+    assert m.BufferExpr.parse("<|>␀\\~", span) == m.BufferExpr(
+        clean_buffer=[],
+        langle=[0, 1, 1, 0],
+        rangle=[0, 1, 1, 0],
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 1, 0, 2],
+    )
+    assert m.BufferExpr.parse("<>␀|\\~", span) == m.BufferExpr(
+        clean_buffer=[],
+        langle=[0, 1, 1, 0],
+        rangle=[0, 1, 1, 0],
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 1, 1, 2],
+    )
+    assert m.BufferExpr.parse("␊", span) == m.BufferExpr(
+        clean_buffer=[""],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=None,
+    )
+    assert m.BufferExpr.parse("|␊", span) == m.BufferExpr(
+        clean_buffer=[""],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 1, 0, 1],
+    )
+    assert m.BufferExpr.parse("|\\␊", span) == m.BufferExpr(
+        clean_buffer=[""],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 1, 0, 2147483647],
+    )
+    assert m.BufferExpr.parse("|\\␊~", span) == m.BufferExpr(
+        clean_buffer=[""],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 1, 0, 1],
+    )
+    assert m.BufferExpr.parse("abc·|def␊", span) == m.BufferExpr(
+        clean_buffer=["abc def"],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 5, 0, 5],
+    )
+    assert m.BufferExpr.parse("<[ab]c·|def\\>␊", span) == m.BufferExpr(
+        clean_buffer=["abc def"],
+        langle=[0, 1, 1, 0],
+        rangle=[0, 1, 8, 0],
+        visual_begin=[0, 1, 1, 0],
+        visual_end=[0, 1, 3, 0],
+        cursor=[0, 1, 5, 0, 2147483647],
+    )
+    assert m.BufferExpr.parse("<[ab]c·|de\\f>␊", span) == m.BufferExpr(
+        clean_buffer=["abc def"],
+        langle=[0, 1, 1, 0],
+        rangle=[0, 1, 8, 0],
+        visual_begin=[0, 1, 1, 0],
+        visual_end=[0, 1, 3, 0],
+        cursor=[0, 1, 5, 0, 7],
+    )
+    assert m.BufferExpr.parse("aa␊|e␊cc␊", span) == m.BufferExpr(
+        clean_buffer=["aa", "e", "cc"],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 2, 1, 0, 1],
+    )
+    assert m.BufferExpr.parse("abc┤~~|~~def␊~~\\~~~gh␊", span) == m.BufferExpr(
+        clean_buffer=["abc\tdef", "gh"],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 4, 3, 15],
+    )
+    assert m.BufferExpr.parse("abc┤~~|~~def\\␊~~~~~gh␊", span) == m.BufferExpr(
+        clean_buffer=["abc\tdef", "gh"],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 4, 3, 12],
+    )
+    assert m.BufferExpr.parse("abc@@|d␊efgh␊", span) == m.BufferExpr(
+        clean_buffer=["abcd", "efgh"],
+        langle=None,
+        rangle=None,
+        visual_begin=None,
+        visual_end=None,
+        cursor=[0, 1, 6, 0, 6],
+    )
+    for invalid_expr in [
+        "␊␀",
+        "␀␊",
+        "abc␀",
+        "␀abc␊",
+        "~abc·def␊",
+        "|abc␊\\def␊",
+        "|abc·def",
+        "\\abc·def␊",
+        "|abc·|def␊",
+        "abc·\\d\\ef␊",
+        "<<abc·def␊",
+    ]:
+        with pytest.raises(m.ParseError):
+            m.BufferExpr.parse(invalid_expr, span)
+
+
+def test_parse_autocmd_event_count_expr():
+    span = m.SourceSpan.for_file("foo").copy_as(3)
+    assert m.AutocmdEventCountExpr.parse(
+        "CmdlineEnter=1", span
+    ) == m.AutocmdEventCountExpr("CmdlineEnter", 1)
+    assert m.AutocmdEventCountExpr.parse(
+        "CmdlineEnter=", span, parse_as_incomplete=True
+    ) == m.AutocmdEventCountExpr("CmdlineEnter", None)
+    with pytest.raises(m.ParseError):
+        m.AutocmdEventCountExpr.parse("CmdlineEnter=", span)
